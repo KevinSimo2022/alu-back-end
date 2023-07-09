c5ee2786af3cb177ce94a93b67bb0a61f8131946
KevinSimo2022 <k.takoulo@alustudent.com>
2023-07-09 19:44:02 +0000

    tasks updated

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""a Python script that, using this REST API"""
+""" Library to gather data from an API """
 
 import requests
 import sys
 
-""" Functions for gathering  data from an API """
+""" Function to gather data from an API """
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
